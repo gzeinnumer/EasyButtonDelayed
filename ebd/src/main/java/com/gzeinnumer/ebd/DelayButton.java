946ae53bf2030ac5a41538b8aadcd79a4bd5195e
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-11-01 19:19:51 +0700

    commit

--- ebd/src/main/java/com/gzeinnumer/ebd/DelayButton.java
+++ ebd/src/main/java/com/gzeinnumer/ebd/DelayButton.java
@@ -1,10 +1,14 @@
 package com.gzeinnumer.ebd;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DelayButton implements View.OnClickListener {
 
@@ -26,6 +30,15 @@ public class DelayButton implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        if (!isTimeAutomatic(view.getContext())) {
+            Toast.makeText(view.getContext(), "Tanggal dan waktu tidak valid, silahkan aktifkan \"Tanggal dan Waktu Otomatis\" serta \"Time Zone Otomatis\" ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!isTimeZoneAutomatic(view.getContext())) {
+            Toast.makeText(view.getContext(), "Tanggal dan waktu tidak valid, silahkan aktifkan \"Tanggal dan Waktu Otomatis\" serta \"Time Zone Otomatis\" ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         onClickListener.onClick(view);
         button.setEnabled(false);
 
@@ -38,5 +51,21 @@ public class DelayButton implements View.OnClickListener {
             }
         };
         countDownTimer.start();
+    }
+
+    private boolean isTimeAutomatic(Context c) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return Settings.Global.getInt(c.getContentResolver(), Settings.Global.AUTO_TIME, 0) == 1;
+        } else {
+            return android.provider.Settings.System.getInt(c.getContentResolver(), Settings.System.AUTO_TIME, 0) == 1;
+        }
+    }
+
+    private boolean isTimeZoneAutomatic(Context c) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return Settings.Global.getInt(c.getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0) == 1;
+        } else {
+            return android.provider.Settings.System.getInt(c.getContentResolver(), Settings.System.AUTO_TIME_ZONE, 0) == 1;
+        }
     }
 }
